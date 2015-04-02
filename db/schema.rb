8c842e36c2bd8a2a0e768370ed2386be253b6f40
mike vallano <mikevallano@gmail.com>
2015-04-02 13:40:33 -0500

    gettin there

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402153632) do
+ActiveRecord::Schema.define(version: 20150402163427) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20150402153632) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
